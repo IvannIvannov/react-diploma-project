511046d3178fb19c1579a53bbbc2c4de45f1ff51
IvannIvannov <151462104+IvannIvannov@users.noreply.github.com>
2026-04-30 11:40:01 +0300

    fix course details to use context instead of service

--- client/src/pages/CourseDetails.tsx
+++ client/src/pages/CourseDetails.tsx
@@ -1,10 +1,10 @@
 import { useParams } from "react-router-dom";
-import { getCourses } from "../services/courseService";
+import { useCourses } from "../context/useCourses";
 
 const CourseDetails = () => {
   const { id } = useParams();
+  const { courses } = useCourses();
 
-  const courses = getCourses();
   const course = courses.find((c) => c.id === id);
 
   if (!course) return <h1>Course not found</h1>;
