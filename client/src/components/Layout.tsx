32ceff1d57dcc0589cdc5f6701fec60cf73b6fb6
IvannIvannov <151462104+IvannIvannov@users.noreply.github.com>
2026-05-27 13:35:09 +0300

    design footer with premium light minimal styling

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
+
       <main style={{ padding: "20px" }}>{children}</main>
+
+      <Footer />
     </>
   );
 };
